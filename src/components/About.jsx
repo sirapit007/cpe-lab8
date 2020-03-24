742c92da68834c10fecd47bc1642fec519ba22a9
sirapit007 <46592400+sirapit007@users.noreply.github.com>
2020-03-24 21:33:27 +0700

    Add files via upload

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export default function About() {
     <div>
       <div className="container mt-5">
         <div className="card">
-          <img src={'https://firebasestorage.googleapis.com/v0/b/lab08-expenses-e4a44.appspot.com/o/IMG_2499.jpg?alt=media&token=bf78edd3-694e-482b-addf-0e14ae806d65'} className='card-img-top' alt="..." />
+          <img src={process.env.PUBLIC_URL+'/img.jpg'} className='card-img-top' alt="..." />
           <div className="card-body">
             <h5 className="card-title">Developer information</h5>
-            <p>Waritthon Auttama(610610612)</p>
+            <p>Sirapit   Leekhao   (610610615)</p>
             <p>This app use Google Firebase as backend.</p>
             <Link to="/">
               <h3>
